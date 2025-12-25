9100ac9c5d166890852b2472cd2279764aaef9ca
zizky13 <zikar.nurizky@gmail.com>
2025-12-25 13:09:45 +0700

    Initial Commit

---
+++ AIMeetingTranscriber/AIMeetingTranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIMeetingTranscriberApp.swift
+//  AIMeetingTranscriber
+//
+//  Created by Zikar Nurizky on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AIMeetingTranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIMeetingTranscriber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIMeetingTranscriber
+//
+//  Created by Zikar Nurizky on 25/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
